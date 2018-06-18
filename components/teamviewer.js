9cb18161431b2293dd5144c9e9ac66ffd4a23703
Eetay Natan <eetay.natan@samanage.com>
2018-06-18 14:32:25 +0300

    cleanup of TeamViewer component

--- components/teamviewer.js
+++ components/teamviewer.js
@@ -13,7 +13,7 @@ export default class TeamViewer extends React.PureComponent {
       response_type: 'code',
       client_id: client_id,
       redirect_uri: redirect_uri,
-      state: '1234',
+      state: platformWidgetHelper.toQueryString({closeWindow: true})
       display: 'popup'
     })
     console.log({teamviewer_url: teamviewer_url})
@@ -27,22 +27,21 @@ export default class TeamViewer extends React.PureComponent {
 }
 
 TeamViewer.getTeamViewerToken = function(event) {
+  // this whole function should be moved to server side
   try {
     var code = event.query_params.code
     var client_id = '163227-2trjZzUFzC6JXp96Wdi2'
     var client_secret = 'afFakDH8scyGwKZNZ72M'
     var xhttp = new XMLHttpRequest()
     xhttp.onreadystatechange = function() {
-      console.log('AJAX STATE', this.status, this.readyState)
       if (this.readyState == 4) {
+        console.log('getTeamViewerToken completed: ', xhttp.responseText)
         if (this.status == 200) {
+          // do some stuff here
         }
-        console.log('AJAX DONE', xhttp.responseText)
-        debugger
       }
     }
     xhttp.open('POST', 'https://webapi.teamviewer.com/api/v1/oauth2/token', true)
-    console.log('AJAX SEND')
     var post_data = platformWidgetHelper.toQueryString({
       grant_type: 'authorization_code',
       code: code,
@@ -51,7 +50,6 @@ TeamViewer.getTeamViewerToken = function(event) {
       client_secret: client_secret
     }).replace(/%20/g, '+')
     xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    debugger
     xhttp.send(post_data)
   }
   catch(e) {
