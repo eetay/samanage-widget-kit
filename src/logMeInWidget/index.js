351ee81c159b6f76d240f0962c3bf6939fc67d9f
Yuli-Arbov <yuli.arbov@samanage.com>
2018-09-17 08:26:25 +0300

    cleanup

--- src/logMeInWidget/index.js
+++ src/logMeInWidget/index.js
@@ -5,14 +5,8 @@ import LogMeInWidget from './LogMeInWidget'
 export default class SamangeWidget extends Component {
   constructor (props) {
     super(props)
-    this.state = { events: [], context: {} }
+    this.state = { context: {} }
   }
-
-  // onWidgetEvent = (event) => {
-  //   const { events } = this.state
-  //   console.log('NEW EVENT:', event)
-  //   this.setState({ events: [...events, event] })
-  // }
 
   onWidgetContextObject = (object) => {
     this.setState({ context: object })
@@ -20,35 +14,17 @@ export default class SamangeWidget extends Component {
 
   componentDidUpdate () {
     const { context } = this.state
-    // platformWidgetHelper.updateHeight()
-    // console.log('DID UPDATE1', context, context.context_type)
     if (context.context_type !== 'Incident') {
       platformWidgetHelper.hide()
-    } else {
-      // platformWidgetHelper.show()
-      // platformWidgetHelper.updateHeight(1500)
-      platformWidgetHelper.updateHeight()
     }
   }
 
   componentDidMount () {
-    // platformWidgetHelper.registerToEvents('*', this.onWidgetEvent)
     platformWidgetHelper.getContextObject(this.onWidgetContextObject)
   }
-  //
-  // renderEvent (event) {
-  //   console.log('EVENT RENDER', event)
-  //   const name = event.eventType || 'JSON event'
-  //   return (
-  //     <div style={{ padding: '3px' }}>
-  //       {React.createElement(ReactJson, { theme: 'monokai', src: event, name, collapsed: '0' })}
-  //     </div>
-  //   )
-  // }
 
   render () {
     const { context } = this.state
-    // console.log('index context=', context)
     if (!context.context_id) return null
     return (
       <div className='slds slds-samanage samanage-media-query'>
